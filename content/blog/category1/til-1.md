d0e4d4bd4093a3dc82baadadab6b333d91b1f0fa
Jisun Lim <jisunlim818@gmail.com>
2020-10-26 16:15:50 +0900

    Init blog project

---
+++ content/blog/category1/til-1.md
@@ -0,0 +1,8 @@
+---
+title: TIL 1
+date: 2020-10-26 15:10:95
+category: category1
+thumbnail: { thumbnailSrc }
+draft: false
+---
+
---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `Jane's Devlog`,
+  description: `Blog posted about ...`,
+  author: `[Jisun Lim]`,
+  introduction: `Carpe diem`,
+  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `https://github.com/janeljs`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: `https://www.facebook.com/jisun153`, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'janeljs',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '', // Add your google analytics tranking ID
+}
