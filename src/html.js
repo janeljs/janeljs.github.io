b72e21d5a5ffced95b6ea0d00fd55da9f3a8dd30
Jisun Lim <jisunlim818@gmail.com>
2020-11-02 19:44:18 +0900

    tag

--- src/html.js
+++ src/html.js
@@ -6,7 +6,6 @@ export default class HTML extends React.Component {
     return (
       <html {...this.props.htmlAttributes}>
         <head>
-          <meta name="msvalidate.01" content="4EA473EFCEF0788D9B1459A98D44C99B" />
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
           <meta
@@ -14,6 +13,7 @@ export default class HTML extends React.Component {
             content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover"
           />
           {this.props.headComponents}
+          <meta name="msvalidate.01" content="4EA473EFCEF0788D9B1459A98D44C99B" />
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
