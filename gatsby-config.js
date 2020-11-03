92a9bcfce20dce1a97a98d8f17f7164f973c8379
Jisun Lim <jisunlim818@gmail.com>
2020-11-04 07:00:52 +0900

    bugfix

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,18 +11,7 @@ module.exports = {
         anonymize: true,
       },
     },
-   // {
-   //  resolve: `gatsby-plugin-gtag`,
-   //  options: {
-   //      // your google analytics tracking id
-   //      trackingId: metaConfig.ga,
-   //      // Puts tracking script in the head instead of the body
-   //      head: true,
-   //      // enable ip anonymization
-   //      anonymize: true,
-   //    },
-   //  },
- 
+   
   {
     resolve: `gatsby-source-filesystem`,
     options: {
