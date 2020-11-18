16f53bbeac4dc1c7146d5cd76c50e56b543715a2
Jisun Lim <jisunlim818@gmail.com>
2020-11-18 17:22:25 +0900

    index.jsx: author-name-content

--- src/components/bio/index.jsx
+++ src/components/bio/index.jsx
@@ -24,7 +24,7 @@ export const Bio = () => (
               />
               <div className="author-name">
                 <span className="author-name-prefix">Written by</span>
-                <Link to={'/about'} className="author-name-content">
+                <Link to={'/about/'} className="author-name-content">
                   <span>@{author}</span>
                 </Link>
                 <div className="author-introduction">{introduction}</div>
