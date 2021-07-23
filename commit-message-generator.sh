d21a82eba544da647c913b0c43553d1c5c994cd3
janeljs <jisunlim818@gmail.com>
2021-07-23 19:23:50 +0900

    feat: 커밋 메시지 자동 생성 스크립트 추가

---
+++ commit-message-generator.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git add .
+C=$(git diff --staged | grep category)
+T=$(git diff --staged | grep title)
+BRANCH=$(git branch | grep '*')
+CATEGORY=${C##*:}
+TITLE=${T##*:}
+ISSUE=${BRANCH##*-}
+MESSAGE="${CATEGORY:1}: (#${ISSUE}) ${TITLE:1}"
+git commit -m "${MESSAGE}"
